5ed0dc4ed52cb9f2c6588195153cd57290d1e157
vatsal51 <vatsal539241@gmail.com>
2022-02-26 15:23:47 +0530

    3 files modified, 1 added

---
+++ Home.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  useEffect(() => {
+    if (localStorage.getItem('Authorization')?.length === 0) {
+      location.href = '/';
+    }
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios
+        .post('https://reqres.in/api/unknown', {
+          headers: {
+            'Content-Type': 'text/plain',
+            Authorization: localStorage.getItem('Authorization'),
+          },
+        })
+        .then((response) => {
+          return response;
+        });
+      console.log(response);
+    } catch (error) {
+      alert('invalid username or passwords');
+    }
+  };
+  return (
+    <div>
+      <h2>Hello</h2>
+    </div>
+  );
+};
